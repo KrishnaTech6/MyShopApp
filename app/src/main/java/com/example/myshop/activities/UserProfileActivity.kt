04ce37bb73df9eb8fea0405ad5ffa8690767c05d
krishnaTech6 <krishna060702@gmail.com>
2023-03-09 22:05:07 +0530

    Storage permission method setup

--- app/src/main/java/com/example/myshop/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/myshop/activities/UserProfileActivity.kt
@@ -1,13 +1,19 @@
 package com.example.myshop.activities
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myshop.R
 import com.example.myshop.models.User
 import com.example.myshop.utils.Constants
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.util.jar.Manifest
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
@@ -26,5 +32,55 @@ class UserProfileActivity : AppCompatActivity() {
 
         et_email_id.isEnabled = false
         et_email_id.setText(userDetails.email)
+
+        iv_photo.setOnClickListener(this@UserProfileActivity)
+    }
+
+    override fun onClick(view: View?) {
+        if (view !=null){
+            when(view.id){
+
+                R.id. iv_photo ->{
+
+                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED){
+                        showErrorSnackBar("You already have the Storage Permission", false)
+                    }
+
+                    else{
+                        //taking permission from the user
+
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+
+
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                showErrorSnackBar("Storage Permission Granted", false)
+            }
+            else{
+                Toast.makeText(this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG).show()
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myshop/utils/Constants.kt
+++ app/src/main/java/com/example/myshop/utils/Constants.kt
@@ -1,9 +1,10 @@
 package com.example.myshop.utils
 
 object Constants {
-    const val USERS: String = "users"
 
+    const val USERS: String = "users"
     const val MYSHOP_PREFERENCES: String = "MyShopPrefs"
     const val LOOGED_IN_USERNAME: String = "logged_in_username"
     const val EXTRA_USER_DETAILS = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE: Int = 2
 }
