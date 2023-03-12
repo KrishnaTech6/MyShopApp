7a73840fa98002014591c709955e2d25b21ed5d3
krishnaTech6 <krishna060702@gmail.com>
2023-03-12 23:17:25 +0530

    Actionbar Settings menu setup and other problems solved (wasn't able to run the app, error in manifest)

---
+++ app/src/main/java/com/example/myshop/ui/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myshop.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myshop.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
--- app/src/main/java/com/example/myshop/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/myshop/ui/fragments/DashboardFragment.kt
@@ -1,13 +1,14 @@
 package com.example.myshop.ui.fragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.myshop.R
 import com.example.myshop.databinding.FragmentDashboardBinding
+import com.example.myshop.ui.activities.SettingsActivity
 
 class DashboardFragment : Fragment() {
 
@@ -16,6 +17,13 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
@@ -27,12 +35,28 @@ class DashboardFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         val textView: TextView = binding.textDashboard
-
         textView.text = "This is dashboard Fragment"
-
         return root
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.settings_action ->{
+                startActivity(Intent(activity, SettingsActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 
     override fun onDestroyView() {
