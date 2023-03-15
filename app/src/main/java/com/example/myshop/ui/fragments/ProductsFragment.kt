e935e04c469b8327c5be409112c0fe2c929b4b7b
krishnaTech6 <krishna060702@gmail.com>
2023-03-15 23:03:14 +0530

    Add product menu added in producs fragment

--- app/src/main/java/com/example/myshop/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/myshop/ui/fragments/ProductsFragment.kt
@@ -1,12 +1,13 @@
 package com.example.myshop.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.myshop.R
 import com.example.myshop.databinding.FragmentProductsBinding
+import com.example.myshop.ui.activities.MainActivity
 
 
 class ProductsFragment : Fragment() {
@@ -14,6 +15,12 @@ class ProductsFragment : Fragment() {
 
     private var _binding: FragmentProductsBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +36,23 @@ class ProductsFragment : Fragment() {
         return root
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_products_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_add_product ->{
+                startActivity(Intent(activity, MainActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
